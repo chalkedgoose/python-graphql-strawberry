6783eb255f02345ed2ad834127650f7efa236cb1
Michael Lavery <michael.lavery.2017@gmail.com>
2022-08-06 23:35:46 -0700

    return to restful

--- src/main.py
+++ src/main.py
@@ -1,16 +1,8 @@
 import asyncio
-
 from distutils.command.upload import upload
 import strawberry
 from strawberry.file_uploads import Upload
 from typing import List, Optional
-
-from dataclasses import asdict
-from distutils.command.upload import upload
-import strawberry
-from strawberry.file_uploads import Upload
-from typing import Dict, List, Optional
-
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse, Response
 
@@ -20,9 +12,7 @@ import requests
 import os
 import cv2
 import psycopg2
-import boto3
-import botocore
-
+import json
 
 engine = psycopg2.connect(
     database=os.getenv("DB_NAME"),
@@ -32,85 +22,35 @@ engine = psycopg2.connect(
     port="5432",
 )
 
-
-s3 = boto3.resource("s3")
-bucket = s3.Bucket("memoimages")
-exists = True
-try:
-    s3.meta.client.head_bucket(Bucket="memoimages")
-except botocore.exceptions.ClientError as e:
-    # If a client error is thrown, then check that it was a 404 error.
-    # If it was a 404 error, then the bucket does not exist.
-    error_code = e.response["Error"]["Code"]
-    if error_code == "404":
-        exists = False
-
-import requests
-import os
-import cv2
-import psycopg2
-
-
-from src.repositories.BookRepo import BookRepo
-from src.repositories.SubmissionsRepo import SubmissionRepo
-from src.repositories.UserRepo import UserRepo
-
-from src.models.User import User
-from src.models.Book import Book
-from src.models.Submission import Submission
-
-
-@strawberry.type
-class Query:
-    @strawberry.field
-    def test_sad(self, text: str) -> str:
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f'Bearer {os.environ["OPENAPI_KEY"]}',
-        }
-        data = (
-            r'{"model": "text-davinci-002", "prompt": "Sentence: \"'
-            + text
-            + r'\"\n% probability of extreme sadness:", "temperature": 0, "max_tokens": 120}'
-        )
-        r = requests.post(
-            "https://api.openai.com/v1/completions", headers=headers, data=data
-        )
-        return r.text
-
-
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    def add_book(self, title: str, author: str) -> str:
-        return Book(title=title, author=author)
-
-
-@strawberry.type
-class Subscription:
-    @strawberry.subscription
-    async def count(self, target: int = 100) -> int:  # type: ignore
-        for i in range(target):
-            yield i
-            await asyncio.sleep(0.5)
-
 app = FastAPI()
 
 
+@app.get("/gpt-3/")
+def test_sad(text: str = "") -> str:
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f'Bearer {os.environ["OPENAPI_KEY"]}',
+    }
+    print(text)
+    data = (
+        r'{"model": "text-davinci-002", "prompt": "Sentence: '
+        + text[1:-1]
+        + r'\nSuicidality from 0 - 100:", "temperature": 0, "max_tokens": 120}'
+    )
+    print(data)
+    r = requests.post(
+        "https://api.openai.com/v1/completions", headers=headers, data=data
+    )
+    pretty = json.loads(r.text)
+    print(pretty["choices"][0]["text"].strip())
+    return pretty["choices"][0]["text"].strip().split()[0]
+
+
 @app.get("/images/{uuid}/{img_id}")
 async def get_image(uuid, img_id):
     img = cv2.imread(img_id)
     res, enc_img = cv2.imencode(".jpg", img)
 
-    return Response(enc_img.tobytes(), media_type="image/jpg")
-
-
-schema = strawberry.Schema(query=Query, mutation=Mutation, subscription=Subscription)
-
-@app.get("/images/{uuid}/{img_id}")
-async def get_image(uuid, img_id):
-    img = cv2.imread(img_id)
-    res, enc_img = cv2.imencode(".jpg", img)
     return Response(enc_img.tobytes(), media_type="image/jpg")
 
 
@@ -123,8 +63,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get("/submissions")
-def read_submissions():
-    return [asdict(x) for x in SubmissionRepo().get_submissions()]
