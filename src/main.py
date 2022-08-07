310906bc3c53126743480868d0de46a97e1e5228
Shadi Bitaraf <bitarafshh@gmail.com>
2022-08-06 17:51:02 -0700

    questionare

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import asyncio
+from dataclasses import asdict
 from distutils.command.upload import upload
 import strawberry
 from strawberry.file_uploads import Upload
-from typing import List, Optional
+from typing import Dict, List, Optional
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse, Response
 
@@ -14,57 +15,14 @@ import os
 import cv2
 import psycopg2
 
+
 from src.repositories.BookRepo import BookRepo
+from src.repositories.SubmissionsRepo import SubmissionRepo
 from src.repositories.UserRepo import UserRepo
+
 from src.models.User import User
 from src.models.Book import Book
-
-@strawberry.type
-class Query:
-    @strawberry.field
-    def books(self, title: Optional[str] = None) -> List[Book]:
-        book_repo = BookRepo()
-
-        if title is not None:
-            return book_repo.get_book_by_title(title)
-
-        return book_repo.get_books()
-
-    @strawberry.field
-    def users(self) -> List[User]:
-        return UserRepo().get_users()
-
-    @strawberry.field
-    def test_sad(self, text: str) -> str:
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f'Bearer {os.environ["OPENAPI_KEY"]}',
-        }
-        data = (
-            r'{"model": "text-davinci-002", "prompt": "Sentence: \"'
-            + text
-            + r'\"\n% probability of extreme sadness:", "temperature": 0, "max_tokens": 120}'
-        )
-        r = requests.post(
-            "https://api.openai.com/v1/completions", headers=headers, data=data
-        )
-        return r.text
-
-
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    def add_book(self, title: str, author: str) -> Book:
-        return BookRepo().create_book(title, author)
-
-
-@strawberry.type
-class Subscription:
-    @strawberry.subscription
-    async def count(self, target: int = 100) -> int:  # type: ignore
-        for i in range(target):
-            yield i
-            await asyncio.sleep(0.5)
+from src.models.Submission import Submission
 
 
 app = FastAPI()
@@ -75,11 +33,6 @@ async def get_image(uuid, img_id):
     img = cv2.imread(img_id)
     res, enc_img = cv2.imencode(".jpg", img)
     return Response(enc_img.tobytes(), media_type="image/jpg")
-
-#make a repo for submissions and models for other features
-schema = strawberry.Schema(query=Query, mutation=Mutation, subscription=Subscription)
-
-graphql_app = GraphQLRouter(schema)
 
 
 origins = ["http://localhost", "http://localhost:8080", "*"]
@@ -92,4 +45,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(graphql_app, prefix="/graphql")
+
+@app.get("/submissions")
+def read_submissions():
+    return [asdict(x) for x in SubmissionRepo().get_submissions()]
